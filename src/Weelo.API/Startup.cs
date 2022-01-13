c7331f5beb4016357c97808d7f62b3da47ae452b
wilson-bg <w.beltran@uniandes.edu.co>
2022-01-13 08:29:28 -0500

    quita referencias sin usar

--- src/Weelo.API/Startup.cs
+++ src/Weelo.API/Startup.cs
@@ -1,19 +1,9 @@
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Weelo.Core.Registers;
 using Weelo.Infrastructure;
 
